7ab9920e040aa8c0a19de7558b06d2e435dfbd1e
mattuu0 <tempacountyt@gmail.com>
2025-01-25 06:02:44 +0000

    ユーザーの情報を設定できるように変更

--- scripts/home.js
+++ scripts/home.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
+let toggle_status = "";
 $(document).ready(function () {
     // トグルスイッチをクリックした時の処理
     $('.toggle_switch').on('click', function () {
@@ -18,11 +19,72 @@ $(document).ready(function () {
     });
 
     // ドロワー内の選択項目をクリックした時の処理
-    $('.toggle_contents p').on('click', function () {
+    $('.toggle_contents p').on('click', async function () {
         const selectedCategory = $(this).text(); // 選択された項目のテキストを取得
         const toggleSwitch = $(this).closest('.toggle_wrap').find('.toggle_switch p');
         toggleSwitch.text(selectedCategory); // トグルスイッチに反映
+
+        // トグルスイッチの状態を更新
+        toggle_status = this.id;
+
         $(this).closest('.toggle_contents').slideUp(); // ドロワーを閉じる
         $(this).closest('.toggle_wrap').find('.toggle_switch').removeClass('open'); // 状態をリセット
+
+
+        const authData = await GetSession();
+
+        const req = await fetch("/app/userStatus/", {
+            method: "POST",
+            headers: {
+                "Authorization": authData["token"],
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "profile": toggle_status
+            })
+        });
+
+        const result = await req.json();
+        console.log(result);
     });
 });
+
+async function Init() {
+    try {
+        // ステータス取得
+        const status = await GetStatus();
+
+        // 話している時
+        if (status["TalkStatus"] == "talking") {
+            // リダイレクト
+            window.location.href = TalkingURL;
+            return;
+        };
+
+        // 認証情報取得
+        const authData = await GetSession();
+
+        const req = await fetch("/app/userStatus/", {
+            method: "GET",
+            headers: {
+                "Authorization": authData["token"],
+            }
+        });
+
+        const result = await req.json();
+
+        // トグルスイッチの状態を更新
+        const toggleSwitch = document.querySelector('.toggle_wrap').querySelector('.toggle_switch p');
+        
+        // トグルスイッチの状態を更新
+        toggleSwitch.textContent = document.getElementById(result["status"]["Status"]).textContent;; // トグルスイッチに反映
+    } catch (error) {
+        console.error(error);
+        // alert("読み取りに失敗しました");
+
+        // ログインに飛ばす
+        // window.location.href = LoginURL;
+    }
+}
+
+Init();
--- scripts/qr_read.js
+++ scripts/qr_read.js
@@ -54,8 +54,18 @@ window.onload = (e) => {
     }
 
     // 読み取るボタンの処理
-    captureButton.onclick = () => {
-        alert("QRコードが読み取られました: " + msg.innerText);
+    captureButton.onclick = async () => {
+        // ロード中にする
+        setLoadText("マッチング中");
+        showLoading();
+
+        await Matching(msg.innerText);
+
+        // ローカルストレージのデータ削除
+        window.localStorage.removeItem("talk_memo");
+
+        // リダイレクト
+        window.location.href = TalkingURL;
     };
 
     // 再読み込みボタンの処理
@@ -67,3 +77,45 @@ window.onload = (e) => {
         startTick(); // 再スタート
     };
 }
+
+async function Matching(token) {
+    const authData = await GetSession();
+
+    const req = await fetch("/app/matching/",{
+        method:"POST",
+        headers : {
+            "Authorization" : authData["token"],
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify({
+            "token" : token
+        })
+    });
+
+    const result = await req.json();
+    console.log(result);
+}
+
+
+async function Init() {
+    try {
+        const status = await GetStatus();
+
+        // 話しているとき
+        if (status["TalkStatus"] == "talking") {
+            // リダイレクト
+            window.location.href = TalkingURL;
+        };
+
+        
+    } catch (ex) {
+        // エラー処理
+        console.error(ex);
+        // alert("読み取りに失敗しました");
+
+        // ログインに飛ばす
+        // window.location.href = LoginURL;
+    }
+}
+
+Init();
